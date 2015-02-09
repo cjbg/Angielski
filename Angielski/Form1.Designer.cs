eb03dbdd05f3a73b0c7b85b303f8eaedc90e4fd4
cjbg <jczabator@gmail.com>
2015-02-09 18:12:20 +0100

    commit

---
+++ Angielski/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Angielski
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.labelUnit = new System.Windows.Forms.Label();
+      this.buttonUnit7 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.buttonUnit5 = new System.Windows.Forms.Button();
+      this.buttonUnit4 = new System.Windows.Forms.Button();
+      this.buttonUnit3 = new System.Windows.Forms.Button();
+      this.buttonUnit2 = new System.Windows.Forms.Button();
+      this.buttonUnit1 = new System.Windows.Forms.Button();
+      this.buttonUnit6 = new System.Windows.Forms.Button();
+      this.buttonUnit12 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(318, 114);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(128, 43);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "angielski -> polski";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(318, 212);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(128, 43);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "polski -> angielski";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(452, 66);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 2;
+      this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(315, 69);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(128, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Liczba powtórzeń słówka";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(72, 69);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(68, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Wybierz unit:";
+      // 
+      // labelUnit
+      // 
+      this.labelUnit.AutoSize = true;
+      this.labelUnit.Location = new System.Drawing.Point(146, 69);
+      this.labelUnit.Name = "labelUnit";
+      this.labelUnit.Size = new System.Drawing.Size(33, 13);
+      this.labelUnit.TabIndex = 5;
+      this.labelUnit.Text = "unit 7";
+      // 
+      // buttonUnit7
+      // 
+      this.buttonUnit7.Location = new System.Drawing.Point(149, 114);
+      this.buttonUnit7.Name = "buttonUnit7";
+      this.buttonUnit7.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit7.TabIndex = 6;
+      this.buttonUnit7.Text = "Unit7";
+      this.buttonUnit7.UseVisualStyleBackColor = true;
+      this.buttonUnit7.Click += new System.EventHandler(this.buttonUnit7_Click);
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(149, 163);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(128, 43);
+      this.button3.TabIndex = 7;
+      this.button3.Text = "Unit8";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(149, 212);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(128, 43);
+      this.button4.TabIndex = 8;
+      this.button4.Text = "Unit9";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button5
+      // 
+      this.button5.Location = new System.Drawing.Point(149, 261);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(128, 43);
+      this.button5.TabIndex = 9;
+      this.button5.Text = "Unit10";
+      this.button5.UseVisualStyleBackColor = true;
+      this.button5.Click += new System.EventHandler(this.button5_Click);
+      // 
+      // button6
+      // 
+      this.button6.Location = new System.Drawing.Point(149, 310);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(128, 43);
+      this.button6.TabIndex = 10;
+      this.button6.Text = "Unit11";
+      this.button6.UseVisualStyleBackColor = true;
+      this.button6.Click += new System.EventHandler(this.button6_Click);
+      // 
+      // buttonUnit5
+      // 
+      this.buttonUnit5.Location = new System.Drawing.Point(15, 310);
+      this.buttonUnit5.Name = "buttonUnit5";
+      this.buttonUnit5.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit5.TabIndex = 15;
+      this.buttonUnit5.Text = "Unit5";
+      this.buttonUnit5.UseVisualStyleBackColor = true;
+      this.buttonUnit5.Click += new System.EventHandler(this.buttonUnit5_Click);
+      // 
+      // buttonUnit4
+      // 
+      this.buttonUnit4.Location = new System.Drawing.Point(15, 261);
+      this.buttonUnit4.Name = "buttonUnit4";
+      this.buttonUnit4.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit4.TabIndex = 14;
+      this.buttonUnit4.Text = "Unit4";
+      this.buttonUnit4.UseVisualStyleBackColor = true;
+      this.buttonUnit4.Click += new System.EventHandler(this.buttonUnit4_Click);
+      // 
+      // buttonUnit3
+      // 
+      this.buttonUnit3.Location = new System.Drawing.Point(15, 212);
+      this.buttonUnit3.Name = "buttonUnit3";
+      this.buttonUnit3.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit3.TabIndex = 13;
+      this.buttonUnit3.Text = "Unit3";
+      this.buttonUnit3.UseVisualStyleBackColor = true;
+      this.buttonUnit3.Click += new System.EventHandler(this.buttonUnit3_Click);
+      // 
+      // buttonUnit2
+      // 
+      this.buttonUnit2.Location = new System.Drawing.Point(15, 163);
+      this.buttonUnit2.Name = "buttonUnit2";
+      this.buttonUnit2.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit2.TabIndex = 12;
+      this.buttonUnit2.Text = "Unit2";
+      this.buttonUnit2.UseVisualStyleBackColor = true;
+      this.buttonUnit2.Click += new System.EventHandler(this.buttonUnit2_Click);
+      // 
+      // buttonUnit1
+      // 
+      this.buttonUnit1.Location = new System.Drawing.Point(15, 114);
+      this.buttonUnit1.Name = "buttonUnit1";
+      this.buttonUnit1.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit1.TabIndex = 11;
+      this.buttonUnit1.Text = "Unit1";
+      this.buttonUnit1.UseVisualStyleBackColor = true;
+      this.buttonUnit1.Click += new System.EventHandler(this.buttonUnit1_Click);
+      // 
+      // buttonUnit6
+      // 
+      this.buttonUnit6.Location = new System.Drawing.Point(15, 359);
+      this.buttonUnit6.Name = "buttonUnit6";
+      this.buttonUnit6.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit6.TabIndex = 16;
+      this.buttonUnit6.Text = "Unit6";
+      this.buttonUnit6.UseVisualStyleBackColor = true;
+      this.buttonUnit6.Click += new System.EventHandler(this.buttonUnit6_Click);
+      // 
+      // buttonUnit12
+      // 
+      this.buttonUnit12.Location = new System.Drawing.Point(149, 359);
+      this.buttonUnit12.Name = "buttonUnit12";
+      this.buttonUnit12.Size = new System.Drawing.Size(128, 43);
+      this.buttonUnit12.TabIndex = 17;
+      this.buttonUnit12.Text = "Unit12";
+      this.buttonUnit12.UseVisualStyleBackColor = true;
+      this.buttonUnit12.Click += new System.EventHandler(this.buttonUnit12_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(653, 436);
+      this.Controls.Add(this.buttonUnit12);
+      this.Controls.Add(this.buttonUnit6);
+      this.Controls.Add(this.buttonUnit5);
+      this.Controls.Add(this.buttonUnit4);
+      this.Controls.Add(this.buttonUnit3);
+      this.Controls.Add(this.buttonUnit2);
+      this.Controls.Add(this.buttonUnit1);
+      this.Controls.Add(this.button6);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.buttonUnit7);
+      this.Controls.Add(this.labelUnit);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Angielski";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label labelUnit;
+    private System.Windows.Forms.Button buttonUnit7;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.Button buttonUnit5;
+    private System.Windows.Forms.Button buttonUnit4;
+    private System.Windows.Forms.Button buttonUnit3;
+    private System.Windows.Forms.Button buttonUnit2;
+    private System.Windows.Forms.Button buttonUnit1;
+    private System.Windows.Forms.Button buttonUnit6;
+    private System.Windows.Forms.Button buttonUnit12;
+  }
+}
+
---
+++ Angielski/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Angielski.Properties;
+
+namespace Angielski
+{
+  public partial class Form1 : Form
+  {
+    private int _repetitionNumber = 2;
+    private Dictionary<string, Word> _wordsAngPol;
+    private Dictionary<string, Word> _wordsPolAng;
+
+    public Form1()
+    {
+      InitializeComponent();
+      ReadTxtFile(Resources.unit7);
+    }
+
+    private void ReadTxtFile(string resource)
+    {
+      string resourceData = resource;
+      List<string> words =
+        resourceData
+        .Split(new[] { Environment.NewLine, "-" }, StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+      _wordsAngPol = new Dictionary<string, Word>();
+      _wordsPolAng = new Dictionary<string, Word>();
+
+      for (int i = 0; i < words.Count - 1; i += 2)
+      {
+        _wordsAngPol.Add(words[i].Trim(),
+          new Word { WordName = words[i + 1].Trim(), RepetitionNumber = _repetitionNumber });
+        _wordsPolAng.Add(words[i + 1].Trim(),
+          new Word { WordName = words[i].Trim(), RepetitionNumber = _repetitionNumber});
+      }     
+    }
+
+    
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Form2 form = new Form2(_wordsAngPol, _repetitionNumber);
+      form.ShowDialog();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      Form2 form = new Form2(_wordsPolAng, _repetitionNumber);
+      form.ShowDialog();
+    }
+
+    private void buttonUnit7_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit7);
+      labelUnit.Text = "unit 7";
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit8);
+      labelUnit.Text = "unit 8";
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit9);
+      labelUnit.Text = "unit 9";
+    }
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit10);
+      labelUnit.Text = "unit 10";
+    }
+
+    private void button6_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit11);
+      labelUnit.Text = "unit 11";
+    }
+
+    private void textBox1_Leave(object sender, EventArgs e)
+    {
+      try
+      {
+        _repetitionNumber = Convert.ToInt32(textBox1.Text);
+        foreach (KeyValuePair<string, Word> word in _wordsAngPol)
+        {
+          word.Value.RepetitionNumber = _repetitionNumber;
+        }
+
+        foreach (KeyValuePair<string, Word> word in _wordsPolAng)
+        {
+          word.Value.RepetitionNumber = _repetitionNumber;
+        }
+      }
+      catch (Exception)
+      {
+        MessageBox.Show(Resources.Form1_textBox1_TextChanged_Zła_wartość);
+      }
+    }
+
+    private void buttonUnit1_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit1);
+      labelUnit.Text = "unit 1";
+    }
+
+    private void buttonUnit2_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit2);
+      labelUnit.Text = "unit 2";
+    }
+
+    private void buttonUnit3_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit3);
+      labelUnit.Text = "unit 3";
+    }
+
+    private void buttonUnit4_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit4);
+      labelUnit.Text = "unit 4";
+
+    }
+
+    private void buttonUnit5_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit5);
+      labelUnit.Text = "unit 5";
+    }
+
+    private void buttonUnit6_Click(object sender, EventArgs e)
+    {
+      ReadTxtFile(Resources.unit6);
+      labelUnit.Text = "unit 6";
+    }
+
+    private void buttonUnit12_Click(object sender, EventArgs e)
+    {
+      MessageBox.Show("Już jest kotku ;*");
+      ReadTxtFile(Resources.unit12);
+      labelUnit.Text = "unit 12";
+    }
+  }
+}
